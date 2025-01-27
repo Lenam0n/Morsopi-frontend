87cd9f3399e58a731c5463bb09fbbbd40288fb65
Lenam0n <lena.wiederstein123@gmail.com>
2025-01-27 21:08:24 +0100

    basis projekt mit react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from "react";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<div>root</div>} />
+      <Route path="/about" element={<div>about</div>} />
+      <Route path="*" element={<div>404Page</div>} />
+    </Routes>
   );
-}
+};
 
 export default App;
